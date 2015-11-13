5b1bbad59999283878be875ec09df4657440c640
Alexander Rulkens <rulkens@gmail.com>
2015-11-13 01:31:25 +0100

    added some examples

--- examples/frequencies.js
+++ examples/frequencies.js
@@ -1,21 +1,29 @@
-var MUSIQ = require('../../src/MUSIQ');
+var MUSIQ = require('../src/MUSIQ');
 
 // from the basic note names
 MUSIQ.noteNames
-     .map(note => MUSIQ.note(note))
-     .map(note => note)
-     .transpose('octave')
-     .transpose('octave')
-     .transpose('octave') // transpose 8 semitones
-     .map(note => logFreq(note));
+    .map(note => MUSIQ.note(note))
+    .map(note => note.toAbsolute())
+    //.map(note => { console.log(note); return note; })
+    .map(note => note.transpose('octave'))
+    .map(note => note.transpose('octave'))
+    .map(note => note.transpose('octave')) // transpose three octaves
+    .map(note => logFreq(note));
 
 // from a scale
-MUSIQ.scale('Cm')
-     .transpose('octave')
-     .noteObjects()
-     .map(note => logFreq(note));
+console.log('C chromatic scale');
+MUSIQ.scale('C chromatic')
+    .transpose('octave')
+    .noteObjects()
+    .map(note => logFreq(note));
 
+// concert pitch
+console.log('concert pitch (A4):', MUSIQ.note('A4').frequency().toFixed(3) + 'Hz')
 // helper functions
-function logFreq(note){
-    console.log(note.notation(), note.frequency().toFixed(2) + ' Hz');
+function logFreq (note) {
+    console.log(padLeft(note.notation(), 3), note.frequency().toFixed(3) + ' Hz');
+}
+
+function padLeft(nr, n, str){
+    return Array(n-String(nr).length+1).join(str||' ')+nr;
 }
--- examples/scales.js
+++ examples/scales.js
@@ -1,3 +1,8 @@
-/**
- * Created by rulkens on 12/11/15.
- */
+var MUSIQ = require('../../src/MUSIQ');
+
+var cScale = MUSIQ.scale('C')
+    .noteObjects()
+    .map(note => note.notation())
+    .reduce((memo, notation) => memo += notation);
+
+console.log('cScale', cScale);
